e2f47fa64dcad328266a3a384ce00ac654ddf381
DeprecatedLuxas <lucasmathias65@icloud.com>
2021-12-25 22:07:59 +0100

    added the rest

--- index.js
+++ index.js
@@ -45,4 +45,42 @@ app.get("/iconmapping/file", async (req, res) => {
   res.json(fileIcons);
 });
 
+app.get("/iconmapping/folder", async (req, res) => {
+  const data = await (
+    await axios.get(
+      "https://api.github.com/repos/Pkief/vscode-material-icon-theme/contents/src/icons/folderIcons.ts",
+      {
+        headers: {
+          Authorization: `token ${process.env.GITHUB_TOKEN}`,
+        },
+      }
+    )
+  ).data;
+  let content = decode(data.content);
+  content = content.substring(
+    content.indexOf("export const folderIcons: FolderTheme[] = ") +
+      "export const folderIcons: FolderTheme[] = ".length
+  );
+
+  content = content
+    .replace(/\n/g, "")
+    .replace(/IconPack.Angular/g, '"angular"')
+    .replace(/IconPack.Nest/g, '"nest"')
+    .replace(/IconPack.Ngrx/g, '"angular_ngrx"')
+    .replace(/IconPack.React/g, '"react"')
+    .replace(/IconPack.Redux/g, '"react_redux"')
+    .replace(/IconPack.Vuex/g, '"vue_vuex"')
+    .replace(/IconPack.Vue/g, '"vue"');
+
+  // For some reason i need to save it to a file and then read it back in.
+  fs.writeFileSync(
+    "./folderIcons.js",
+    "const folderIcons = " + content + "\n\nmodule.exports = folderIcons;"
+  );
+
+  const fileIcons = require("./folderIcons.js");
+
+  res.json(fileIcons);
+});
+
 app.listen(5000, () => console.log("Server started on port 5000"));
